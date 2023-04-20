d3812a42a27303b4fb94049ccf5b8ee3efe867b6
كارل مبارك <bonjour@moubarak.eu>
2023-04-20 15:03:46 +0200

    SECRETS: moved secrets out of LocalSettings.php and created sample file

---
+++ .secrets.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+# Secret strings to include in LocalSwettings.php. First,
+# copy this file to a directory outside your webroot and then
+# require_once this file in your LocalSettings.php
+
+$wgDBtype = "";
+$wgDBserver = "";
+$wgDBname = "";
+$wgDBuser = "";
+$wgDBpassword = "";
+$wgSecretKey = "";
+
+?>
